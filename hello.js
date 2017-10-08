4a378b5bac788d2af4ede5e0acb67d9dad70f279
Clay Sissing <clay@mylocalpitch.com>
2017-10-08 17:09:52 +0100

    changes

--- hello.js
+++ hello.js
@@ -2,6 +2,6 @@
 var http = require('http');
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World deployment tester restart\n');
+  res.end('Hello World deployment tester restart 2\n');
 }).listen(8080, 'localhost');
 console.log('Server running at http://localhost:8080/');
